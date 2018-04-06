9918753ec820d284b9ac40b49dc81827c33ba4e9
Ezdehar Jaber <ezdeharjaber@Ezdehars-MacBook-Pro.local>
2018-04-06 15:29:50 -0400

    yes

---
+++ liri.js
@@ -0,0 +1,19 @@
+require("dotenv").config(); 
+
+var keys = require("./keys.js");
+
+console.log(process.env);
+
+//function Exports(key) {
+
+//var spotify = new Spotify(keys.spotify);
+//var client = new Twitter(keys.twitter);
+
+//this.printKey = function () {
+//	console.log("\nKey:" + this.key);
+//	}
+//module.exports = {
+//	APIKey: APIKey
+//};
+
+//};
