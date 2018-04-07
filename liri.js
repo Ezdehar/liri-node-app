9f88851c0786c8e1ec160b3d89f8842cb254edd6
Ezdehar Jaber <ezdeharjaber@Ezdehars-MacBook-Pro.local>
2018-04-06 22:31:17 -0400

    update

--- liri.js
+++ liri.js
@@ -1,24 +1,75 @@
 require("dotenv").config(); 
 
-var fs = require("./keys.js", "utf8", function(err, data) {
-		//var keys = require
-	if (err) {
-		return console.error(err);
-	}
-	console.log("this is uploaded"); 
+var keys = require("./keys.js");
 
-	var spotify = new Spotify(keys.spotify);
-	var client = new Twitter(keys.twitter);
-	var keys = process.env;
-	
-	for (var i = 0; i < keys.length; i++) {
-		
-		console.log(keys[i]);
+
+var Twitter = require("twitter");
+
+//var client = new Twitter(keys.twitter);
+//var spotify = new Spotify(keys.spotify);
+
+var getTweets = function(myTweets) {
+  if(error) {
+    console.log(error)
+  }
+
+  var client = new Twitter(keys.twitter);
+
+  var pick = function(caseData, functionData) {
+  switch (caseData) {
+    case "my-tweets":
+      getMyTweets();
+      break;
+    case "spotify-this-song":
+      getMeSpotify(functionData);
+      break;
+    case "movie-this":
+      getMovie(functionData);
+      break;
+    case "do-what-it-says":
+      doWhatItSays();
+      break;
+    default:
+      console.log("LIRI doesn't know that");
+  }
+}
+
+var runThis = function(argOne, argTwo) {
+  pick(argOne, argTwo);
+};
+
+runThis(process.argv[2], process.argv[3]);
+}
+
+var request = require("request");
+
+var getMovie = function(movieName) {
+	if (movieName === undefined) {
+		movieName = "Mr. Nobody.";
 	}
-	
-});
+
+	var movieUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy"
+
+	request(movieUrl, function(error, response, body){
+		if (!error && response.statusCode === 200) {
+			var data = JSON.parse(body)
+
+			//console.log("data ================ >", data)
+			console.log("year: ", data.Year)
+		}
+		if (error) {
+			console.log(error)
+		}
+	})
+} 
+
+
+
+//http://www.omdbapi.com/?t=Titanic
+
 
 //function Exports(key) {
+
 
 //console.log("\nKey:" + this.key);
 
