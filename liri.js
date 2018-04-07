a74ee3e0a6f3245a00e53c4dec551af13082c878
Ezdehar Jaber <ezdeharjaber@Ezdehars-MacBook-Pro.local>
2018-04-07 10:20:13 -0400

    update

--- liri.js
+++ liri.js
@@ -3,44 +3,6 @@ require("dotenv").config();
 var keys = require("./keys.js");
 
 
-var Twitter = require("twitter");
-
-//var client = new Twitter(keys.twitter);
-//var spotify = new Spotify(keys.spotify);
-
-var getTweets = function(myTweets) {
-  if(error) {
-    console.log(error)
-  }
-
-  var client = new Twitter(keys.twitter);
-
-  var pick = function(caseData, functionData) {
-  switch (caseData) {
-    case "my-tweets":
-      getMyTweets();
-      break;
-    case "spotify-this-song":
-      getMeSpotify(functionData);
-      break;
-    case "movie-this":
-      getMovie(functionData);
-      break;
-    case "do-what-it-says":
-      doWhatItSays();
-      break;
-    default:
-      console.log("LIRI doesn't know that");
-  }
-}
-
-var runThis = function(argOne, argTwo) {
-  pick(argOne, argTwo);
-};
-
-runThis(process.argv[2], process.argv[3]);
-}
-
 var request = require("request");
 
 var getMovie = function(movieName) {
@@ -50,7 +12,7 @@ var getMovie = function(movieName) {
 
 	var movieUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy"
 
-	request(movieUrl, function(error, response, body){
+	request(movieUrl, function(error, response, body) {
 		if (!error && response.statusCode === 200) {
 			var data = JSON.parse(body)
 
@@ -61,22 +23,54 @@ var getMovie = function(movieName) {
 			console.log(error)
 		}
 	})
-} 
+}
+
+var Twitter = require("twitter");
+
+  var client = new Twitter(keys.twitter);
+    // consumer_key: "",
+    // consumer_secret: "",
+    // access_token_key: "",
+    // access_token_secret: ""
+
+client.get("favorites/list", function(error, tweets, response) {
+  if (!error && response.statusCode === 20) {
+      var data = JSON.parse(body)
+
+      console.log("my-tweets")
+      //console.log(response);
+    // }
+    // if (error) {
+    //     console.log(error)
+    // }
+
+var pick = function(caseData, functionData) {
+  switch (caseData) {
+    case "my-tweets":
+      //getMyTweets();
+      break;
+    case "spotify-this-song":
+      //getMeSpotify(functionData);
+      break;
+    case "movie-this":
+      //getMovie(functionData);
+      break;
+    case "do-what-it-says":
+      //doWhatItSays();
+      break;
+    default:
+      console.log("LIRI doesn't know that");
+  }
+};
+
+var runThis = function(argOne, argTwo) {
+  pick(argOne, argTwo);
+};
+
+runThis(process.argv[2], process.argv[3]);
+});
 
 
 
-//http://www.omdbapi.com/?t=Titanic
 
 
-//function Exports(key) {
-
-
-//console.log("\nKey:" + this.key);
-
-//this.printKey = function () {
-//	
-//module.exports = {
-//	APIKey: APIKey
-//};
-
-//};
